9eb54e3967e2bbb6903d07508530979bf3b6ea32
lyabs243 <loicyabili@outlook.com>
2019-07-25 20:36:34 +0200

    add feed item class

---
+++ lib/models/feed.dart
@@ -0,0 +1,14 @@
+
+class Feed{
+
+  String channelTitle;
+  String articleTitle;
+  String articleDescription;
+  String articlePubDate;
+  String articleUurlImage;
+  String articleUrlArticle;
+
+  Feed(this.channelTitle, this.articleTitle, this.articleDescription,
+      this.articlePubDate, this.articleUurlImage, this.articleUrlArticle);
+
+}
